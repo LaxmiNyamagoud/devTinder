16a1f30c312c483fc11528d539cffb4dce7b75dc
Laxmi Nyamagoud <laxmi.nyamagoud@intimetec.com>
2025-12-28 19:10:33 +0530

    added http methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ const express = require("express")
 
 const app = express(); //Create an Express application
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-}); //Start the server on port 3000
+//This will handle GET requests to the /user path
+app.get('/user',(req,res)=>{
+    console.log("User route accessed");
+    res.send({message: "User route accessed successfully"});
+})
 
-app.use('/',(req,res)=>{
-   res.send("Hello, World! Dashboard.");
+app.post('/user',(req,res)=>{
+    res.send({message: "User created successfully"});
+})
+
+app.delete('/user',(req,res)=>{
+    res.send({message: "User deleted successfully"});
 })
 
 app.use('/test',(req,res)=>{
 res.send("This is a test route.");
 })
+
+//This will match all the http method api calls 
+app.use('/',(req,res)=>{
+   res.send("Hello, default path.");
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+}); //Start the server on port 3000
