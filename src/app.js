089ea2d29f6074eb1c808ba33276314e69641c80
Laxmi Nyamagoud <laxmi.nyamagoud@intimetec.com>
2025-12-29 11:21:49 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,33 +2,14 @@ const express = require("express")
 
 const app = express(); //Create an Express application
 
-//This will handle GET requests to the /user path
-app.get('/user',(req,res)=>{
-    console.log("User route accessed");
-    res.send({message: "User route accessed successfully"});
-})
+app.use('/user',(req,res,next)=>{
+    console.log("User endpoint hit");
+    next();
+    // res.send("User endpoint response");
+},(req,res)=>{
+    console.log("Second middleware for /user 2");
+    res.send("User endpoint response 2");
 
-//Dynamic route to handle userId parameter
-app.get('/user/:userId',(req,res)=>{
-    console.log(req.params);
-    res.send({message: "User route accessed successfully"});
-})
-
-app.post('/user',(req,res)=>{
-    res.send({message: "User created successfully"});
-})
-
-app.delete('/user',(req,res)=>{
-    res.send({message: "User deleted successfully"});
-})
-
-app.use('/test',(req,res)=>{
-res.send("This is a test route.");
-})
-
-//This will match all the http method api calls 
-app.use('/',(req,res)=>{
-   res.send("Hello, default path.");
 })
 
 app.listen(3000,()=>{
