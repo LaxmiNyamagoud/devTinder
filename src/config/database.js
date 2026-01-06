4b6c1e384bc741100494efa2af6991c81923b8e6
Laxmi Nyamagoud <laxmi.nyamagoud@intimetec.com>
2026-01-06 16:14:09 +0530

    removed cred

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://learningnodeitt:eGlqSN7pyoiY4UKT@learningnode.scjeoxw.mongodb.net/devTinder')
-};
-
-module.exports = connectDB;
